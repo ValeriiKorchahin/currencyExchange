c0478be3ab4618ce8813a3d53d145ffb1f406d8d
ValeriiKorchahin <valerakorchagin1512@gmail.com>
2022-10-31 16:34:44 +0200

    initial commit

--- src/app/components/exchanger/exchanger.component.ts
+++ src/app/components/exchanger/exchanger.component.ts
@@ -13,45 +13,71 @@ export class ExchangerComponent implements OnInit {
   secondSelectedCurrency = 'UAH';
 
   //Values of input controllers
-  firstInputValue = 1;
-  secondInputValue = 1;
+  firstInputValue: any = 1;
+  secondInputValue: any = 1;
 
   constructor(private currencyRateService: CurrencyRateService) {
   }
 
   ngOnInit(): void {
     this.currencyRateService.getExchange(this.firstSelectedCurrency, this.secondSelectedCurrency).subscribe(({info}) => {
-      this.secondInputValue = +(this.firstInputValue * info.rate).toFixed(4)
+      if (isNaN(this.firstInputValue)){
+        this.secondInputValue = 'Error';
+      } else {
+        this.secondInputValue = +(this.firstInputValue * info.rate).toFixed(4)
+      }
     })
+
     this.currencyRateService.getExchange(this.secondSelectedCurrency, this.firstSelectedCurrency).subscribe(({info}) => {
-      this.firstInputValue = +(this.secondInputValue * info.rate).toFixed(4);
+      if (isNaN(this.secondInputValue)){
+        this.firstInputValue = 'Error';
+      } else {
+        this.firstInputValue = +(this.secondInputValue * info.rate).toFixed(4);
+      }
     })
+
   }
 
   firstInputController(value: number) {
     this.firstInputValue = value;
-    this.currencyRateService.getExchange(this.firstSelectedCurrency, this.secondSelectedCurrency).subscribe(({info}) => {
-      this.secondInputValue = +(this.firstInputValue * info.rate).toFixed(4);
-    })
+    if(isNaN(this.firstInputValue)){
+      this.secondInputValue = 'Error';
+    } else {
+      this.currencyRateService.getExchange(this.firstSelectedCurrency, this.secondSelectedCurrency).subscribe(({info}) => {
+        this.secondInputValue = +(this.firstInputValue * info.rate).toFixed(4);
+      })
+    }
   }
 
   secondInputController(value: number) {
     this.secondInputValue = value;
-    this.currencyRateService.getExchange(this.secondSelectedCurrency, this.firstSelectedCurrency).subscribe(({info}) => {
-      this.firstInputValue = +(this.secondInputValue * info.rate).toFixed(4);
-    })
+    if(isNaN(this.secondInputValue)){
+      this.firstInputValue = 'Error';
+    } else {
+      this.currencyRateService.getExchange(this.secondSelectedCurrency, this.firstSelectedCurrency).subscribe(({info}) => {
+        this.firstInputValue = +(this.secondInputValue * info.rate).toFixed(4);
+      })
+    }
   }
 
   firstCurrencyControllerChange(value: string) {
-    this.currencyRateService.getExchange(this.secondSelectedCurrency, value).subscribe(({info}) => {
-      this.firstInputValue = +(this.secondInputValue * info.rate).toFixed(4);
-    })
+      this.currencyRateService.getExchange(this.secondSelectedCurrency, value).subscribe(({info}) => {
+        if (isNaN(this.secondInputValue)) {
+          this.firstInputValue = 'Error';
+        } else {
+          this.firstInputValue = +(this.secondInputValue * info.rate).toFixed(4);
+        }
+      })
   }
 
   secondCurrencyControllerChange(value: string) {
-    this.currencyRateService.getExchange(this.firstSelectedCurrency, value).subscribe(({info}) => {
-      this.secondInputValue = +(this.firstInputValue * info.rate).toFixed(4);
-    })
+      this.currencyRateService.getExchange(this.firstSelectedCurrency, value).subscribe(({info}) => {
+        if (isNaN(this.firstInputValue)) {
+          this.secondInputValue = 'Error';
+        } else {
+          this.secondInputValue = +(this.firstInputValue * info.rate).toFixed(4);
+        }
+      })
   }
 
 }
